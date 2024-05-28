544ee78599af69e645a3673c8e622214183a0b03
Latham Jeppson <lljeppson7@gmail.com>
2024-05-27 22:30:46 -0600

    quick edits

--- app/about/feature.tsx
+++ app/about/feature.tsx
@@ -21,17 +21,18 @@ const Feature = () => {
             The inspiration for Damai Global began with the friendships that our founder, 
             Matthew, forged with ambitious and talented individuals while living in Ghana, Africa. 
             Moved by their potential and drive, Matthew sought to provide them with opportunities 
-            for a more fulfilling and enriching professional journey. Partnering with Carl Van Gils, 
-            a podiatrist at the Foot & Ankle Institute in St. George, Utah, they decided to test the 
-            idea of using a remote offshore worker to support the busy medical practice.
+            for a more fulfilling and enriching professional journey.
+            Partnering with Dr. Carl Van Gils, a podiatrist at the Foot & Ankle Institute in St. George, Utah, 
+            they decided to test the idea of using a remote offshore worker to support the busy medical practice.<br />
             Their first hire was a skilled college graduate from Accra, Ghana. This step proved to 
             be life-changing for the Ghanaian worker and brought significant efficiency and cost 
-            savings to the podiatry practice. Within three months, Dr. Van Gils’ office manager 
-            requested two additional workers, leading to the birth of Damai Global.
+            savings to the podiatry practice.
+            Within three months, Dr. Van Gils’ office manager requested two additional workers, 
+            leading to the birth of Damai Global.<br />
             Founded on the principles of connecting talented individuals worldwide with meaningful 
-            opportunities in the United States, Damai Global has grown rapidly. Today, we assist 
-            businesses in multiple states, positively impacting the lives of motivated and skilled 
-            individuals across the globe.
+            opportunities in the United States, Damai Global has grown rapidly.
+            Today, we assist businesses in multiple states, positively impacting the lives of motivated 
+            and skilled individuals across the globe.
           </p>
           <div className="not-prose flex items-center gap-2">
             <Button className="w-fit" asChild>
